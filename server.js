6e7e99e0cb1812ea9867aaa2ff6ed092d7e5082f
Abhishek Baital <abhishekbaital007@gmail.com>
2023-07-24 16:18:20 +0530

    New commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const colors = require('colors')
 const dotenv = require('dotenv')
 const connectDB = require("./config/db")
+const path = require('path')
 
 //env config
 dotenv.config()
@@ -27,6 +28,13 @@ app.use(morgan('dev'))
 //routes
 app.use("/api/v1/user",userRoutes)
 app.use("/api/v1/blog",blogRoutes)
+
+//static files
+app.use(express.static(path.join(__dirname,"./client/build")))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+})
 
 //PORT
 const PORT = process.env.PORT || 8080
